1f087810506e3f6266f333078ae6a48377979a57
vmalkauthekar <38283717+vmalkauthekar@users.noreply.github.com>
2018-04-13 14:26:06 +0530

    Create AppTest.java

---
+++ src/test/java/com/qaagility/controller/AppTest.java
@@ -0,0 +1,22 @@
+public class AppTest {
+
+Calculator myCalc = new Calculator();
+
+      public void testSlow() {
+        System.out.println("slow");
+      }
+
+      public void testSlower() {
+        System.out.println("slower");
+      }
+
+      public void testFast() {
+        System.out.println("fast-updated");
+      }
+	
+	@Test	
+      public void testCalc() {
+        assertEquals("Result", 9, myCalc.add(3,6));
+      }
+
+    }
